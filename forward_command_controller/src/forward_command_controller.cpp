4410ca6eca5093af430053cc3c9c3815325e8e2f
Denis Štogl <destogl@users.noreply.github.com>
2020-11-21 10:05:47 +0100

    Update forward_command_controller/src/forward_command_controller.cpp
    Co-authored-by: Jordan Palacios <j.palacios1986@gmail.com>

--- forward_command_controller/src/forward_command_controller.cpp
+++ forward_command_controller/src/forward_command_controller.cpp
@@ -66,7 +66,7 @@ ForwardCommandController::command_interface_configuration() const
   command_interfaces_config.type = controller_interface::configuration_type::INDIVIDUAL;
 
   for (const auto & joint : joint_names_) {
-    for (const auto interface : interfaces_) {
+    for (const auto & interface : interfaces_) {
       command_interfaces_config.names.push_back(joint + "/" + interface);
     }
   }
